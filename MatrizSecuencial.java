a7caee364a5d344497f94f0a6a9c0f9aa2393945
juanjosepinedafranco7 <juanjosepinedafranco5@gmail.com>
2026-02-16 21:53:46 -0500

    Implement MatrizSecuencial to create a 3x3 matrix

---
+++ MatrizSecuencial.java
@@ -0,0 +1,14 @@
+public class MatrizSecuencial {
+    public static void main(String[] args) {
+        int[][] matriz = new int[3][3];
+        int numero = 1;
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] = numero++;
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
